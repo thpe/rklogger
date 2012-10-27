5afdadbfee01702f92df68fff5bc3d006015ce9e
Thomas Petig <thomas@petig.eu>
2012-10-27 17:40:20 +0200

    Fixed help message output/README

--- src/main.C
+++ src/main.C
@@ -44,7 +44,7 @@ int main (int argc, char** argv)
         boost::program_options::options_description desc("Allowed options");
         desc.add_options()
             ("help,h", "produce help message")
-            ("device,d", boost::program_options::value<std::string>(&device)->default_value(SERIAL_DEVICE), "produce help message")
+            ("device,d", boost::program_options::value<std::string>(&device)->default_value(SERIAL_DEVICE), "change the communication device")
         ;
 
         boost::program_options::variables_map vm;
