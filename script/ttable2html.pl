f3eafb36e0ce8adddb6c921cc645c273a9b88b53
Thomas Petig <thomas@petig.eu>
2013-02-17 19:07:49 +0100

    added ttable 2 html script

---
+++ script/ttable2html.pl
@@ -0,0 +1,63 @@
+#!/usr/bin/perl
+# Copyright 2012 Thomas Petig
+#
+# This file is part of rklogger.
+#
+# rklogger is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# rklogger is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with rklogger.  If not, see <http://www.gnu.org/licenses/>.
+
+use strict;
+use warnings;
+use Text::CSV;
+use FindBin;
+use Scalar::Util qw(looks_like_number);
+
+my $csv = Text::CSV->new();
+my $line = <>;
+$csv->parse($line);
+my @columns = $csv->fields();
+my $net = $columns[0];
+my $sub = $columns[1];
+my $year = $columns[2];
+my $last_prod = 0;
+my $last = 0;
+
+while (<>) {
+    if ($csv->parse($_)) {
+        my @columns = $csv->fields();
+        print "<TR>";
+        print "<TH>$year</TH>";
+        ++$year;
+        my $tot_prod = 0;
+        if (looks_like_number($columns[-1])) {
+            $tot_prod = $columns[-1] - $last_prod;
+            $last_prod = $columns[-1];
+        }
+        print "<TH>$tot_prod</TH>";
+
+        for (my $i = 0; $i < 12; ++$i) {
+            my $val = $columns[$i];
+            if (!looks_like_number($val)) {
+                $val = $last;
+            } else {
+                $last = $val;
+            }
+            print "<TH>$val</TH>";
+        }
+        print "<TR>\n";
+    } else {
+        my $err = $csv->error_input;
+        print "Failed to parse line: $err";
+    }
+}
+print "\n";
